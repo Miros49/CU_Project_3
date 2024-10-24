8c3aac8de5304c399919b0ad1a9f818483114e3d
Miros49 <4miros9@gmail.com>
2024-10-24 22:16:57 +0300

    readme.md

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from app import create_app
+from flask import Flask
+from app.routes import weather_blueprint, errors_blueprint
 
-app = create_app()
+app = Flask(__name__, template_folder='app/templates', static_folder='app/static', static_url_path='/static')
+
+app.register_blueprint(weather_blueprint)
+app.register_blueprint(errors_blueprint)
 
 if __name__ == '__main__':
     app.run(debug=True)
